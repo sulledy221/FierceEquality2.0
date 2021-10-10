fa0df83fb525c32559f2c15c940539744009614c
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-10-10 16:45:37 -0700

    setting up the navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./all.css";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/Emergency">Emergency</Link>
+      <Link to="/About">About</Link>
+    </div>
+  );
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./all.css";
 
 export default function Posts() {
   return <div></div>;
