c5d5842c8d29b1dce2316ee9c37a1e70d4ed58bf
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-10-11 17:42:53 -0700

    images rendering on the page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,10 +7,34 @@ export default function HomePage({ flagItems }) {
   // const [choice, setChoice] = useState("");
   return (
     <div>
-      <img src="/assets/FLAGS/Asexual.jpeg" alt="" className="div-flag" />
-      <img src="/assets/FLAGS/bisexual.jpeg" alt="" className="div-flag" />
-      <img src="/assets/FLAGS/trans.jpeg" alt="" className="div-flag" />
-      <img src="/assets/FLAGS/rainbow.jpeg" alt="" className="div-flag" />
+      <li className="list-flag">
+        <img
+          src="/assets/FLAGS/Asexual.jpeg"
+          alt="Asexual Pride Flag"
+          className="img-flag"
+        />
+      </li>
+      <li className="list-flag">
+        <img
+          src="/assets/FLAGS/bisexual.jpeg"
+          alt="Bisexual Pride Flag"
+          className="img-flag"
+        />
+      </li>
+      <li className="list-flag">
+        <img
+          src="/assets/FLAGS/trans.jpeg"
+          alt="Transgender Pride Flag"
+          className="img-flag"
+        />
+      </li>
+      <li className="list-flag">
+        <img
+          src="/assets/FLAGS/rainbow.jpeg"
+          alt="Gay Pride Flag"
+          className="img-flag"
+        />
+      </li>
     </div>
   );
 }
