db9f69bdbb54ac47cd6bf42584a74d09309dde6c
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-10-14 18:21:16 -0700

    all done

--- src/components/Flags.jsx
+++ src/components/Flags.jsx
@@ -3,7 +3,7 @@ import { flagData } from "./flagdata";
 export default function Flags() {
   const renderFlags = flagData.map((item) => (
     <li key={item.alt}>
-      <p className="list-flag">{item.title}</p>
+      <p className="list-flag margin">{item.title}</p>
       <p>{item.desc}</p>
       <img src={item.src} alt={item.alt} className="img-flag" />
       <div>
